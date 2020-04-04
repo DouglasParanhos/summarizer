8f3469cefd108126cf0404f6ae61167fe850a1b9
Paranhos <douglasfaparanhos@gmail.com>
2020-04-04 17:53:10 -0300

    Initial components in frontend

---
+++ summarizer-front/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import InputForm from './Components/InputForm';
+
+function App() {
+  return (
+    <div className="principal">
+      <h1> Summarizer </h1>
+      <InputForm />
+    </div>
+  );
+}
+
+export default App;
